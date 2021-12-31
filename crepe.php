c7e412a0cce42d3fee72f3f2b009197844a39eed
Aurelie.Bergey <aurelie.bergey@gmail.com>
2021-12-31 15:03:19 +0100

    ?? NEW: mise a jour 1

--- crepe.php
+++ crepe.php
@@ -1,5 +1,17 @@
 <?php
 
 class crepe {
+    private $farine ="500g de farine";
+    private $lait= "1l de lait";
+    private $oeuf="5 oeufs";
+    private $bière="25cl de bière";
+    private $sucreVanille= "1 sachet de sucre vanillé";
+    private $sucre= "50g de sucre";
+    private $rhum = "3 cuilleres à soupe de rhum";
+    private $beurre = "quantité necessaire de beurre pour mettre dans la poèle ";
+
+    function Afficher_recette (){
+        echo "Pour faire une pate a crepe il te faut:/n - $farine /n  -$lait /n -$oeuf /n -$bière /n - $sucreVanille /n - $sucre /n -$rhum /n -$beurre";
+    }
     
 }
